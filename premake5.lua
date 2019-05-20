246e00f70087fcaf2cd5d765785b7224f1e14278
Alexander Wester <piratkopia13@gmail.com>
2019-05-20 21:49:29 +0200

    Added premake file

---
+++ premake5.lua
@@ -0,0 +1,28 @@
+-- premake5.lua
+project "ImGui"
+	kind "StaticLib"
+	language "C++"
+	
+	targetdir (binDir)
+	objdir (intermediatesDir)
+
+	files {
+		"imconfig.h",
+		"imgui.h",
+		"imgui.cpp",
+		"imgui_draw.cpp",
+		"imgui_internal.h",
+		"imgui_widgets.cpp",
+		"imstb_rectpack.h",
+		"imstb_textedit.h",
+		"imstb_truetype.h",
+		"imgui_demo.cpp"
+	}
+	
+	filter "system:windows"
+		systemversion "latest"
+		cppdialect "C++17"
+		staticruntime "On"
+		
+	filter { "system:windows", "configurations:Release" }
+		buildoptions "/MT"
